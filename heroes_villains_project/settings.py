7081ab06e246e770d19c589976d9d680869c1062
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-11 15:50:12 -0500

    changed database settings + created mysql database and ran migrations

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*l%q0h8p%g4k2skr(f@o+9mk7=y3&zsz1trv84*!n7f)kptxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -109,7 +98,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -121,3 +110,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
