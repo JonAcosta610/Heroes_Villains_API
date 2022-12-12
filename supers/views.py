bc0b3197b1448613b442495462f511ab8dba2d7f
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-12 00:31:58 -0500

    created postman collection + created GET HTTP command on postman + added serializers to product models views file

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Supers
 
 # Create your views here.
+@api_view(['GET'])
+def supers_list(request):
+    supers = Supers.objects.all()
+    serializer = SuperSerializer(supers, many=True)
+    return Response(serializer.data)
