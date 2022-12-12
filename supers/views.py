50dfac2e6f443e7cddd6465d19736c51358cc36c
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-12 07:00:39 -0500

    created new view function for getting supers by id + added new urls path for supers detail view function

--- supers/views.py
+++ supers/views.py
@@ -2,10 +2,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SuperSerializer
 from .models import Supers
+from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Supers.objects.all()
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def supers_detail(request, pk):
+    try:
+        supers = Supers.objects.get(pk=pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    except Supers.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
