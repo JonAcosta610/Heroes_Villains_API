23bf465215229c9e121aae6245f5d585b60f723f
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-13 18:24:35 -0500

    created new view function for getting type by id + added a new urls path for type detail view function

--- super_type/views.py
+++ super_type/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import SuperTypeSerializer
 from .models import SuperType
-# from rest_framework import status
+from rest_framework import status
 # from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -11,3 +11,12 @@ def type_list(request):
     type = SuperType.objects.all()
     serializer = SuperTypeSerializer(type, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def type_detail(request, pk):
+    try:
+        type = SuperType.objects.get(pk=pk)
+        serializer = SuperTypeSerializer(type)
+        return Response(serializer.data)
+    except SuperType.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
