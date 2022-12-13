ac9c729d6c669a9fd4eff6e2646b0ad80c8c5e59
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-13 15:30:15 -0500

    created postman collection + created get HTTP command on postman + added serializer to type models views file

--- super_type/views.py
+++ super_type/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import SuperSerializer
+from .serializer import SuperTypeSerializer
 from .models import SuperType
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+# from rest_framework import status
+# from django.shortcuts import get_object_or_404
 
 # Create your views here.
+@api_view(['GET'])
+def type_list(request):
+    type = SuperType.objects.all()
+    serializer = SuperTypeSerializer(type, many=True)
+    return Response(serializer.data)
